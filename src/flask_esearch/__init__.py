18dd713d628b0cd1eca9ef95b6821b8b30a68f0b
filak <filak@users.noreply.github.com>
2023-10-13 00:44:54 +0200

    Removing _app_ctx_stack

--- src/flask_esearch/__init__.py
+++ src/flask_esearch/__init__.py
@@ -1,6 +1,17 @@
+from types import SimpleNamespace
+from typing import Optional
 from elasticsearch import Elasticsearch
 from flask import Flask
-from flask import _app_ctx_stack as stack
+from flask import g, current_app
+
+def _get_current_context() -> Optional[SimpleNamespace]:
+    if not g:
+        return None
+
+    if not hasattr(g, '_flask_esearch'):
+        g._flask_esearch = SimpleNamespace()
+
+    return g._flask_esearch
 
 
 class ESearch:
@@ -37,12 +48,12 @@ class ESearch:
 
         app.teardown_appcontext(self.teardown)
 
-    def configure_app(self, context):
+    def configure_app(self, current_app):
         """
         Validates the setting if exists and raise an Exception if not properly
         configured
         """
-        hosts = context.app.config.get('ELASTICSEARCH_HOST')
+        hosts = current_app.config.get('ELASTICSEARCH_HOST')
 
         if not isinstance(hosts, (str, list)):
             raise ValueError(f"A __str__ or list is expected and not {type(hosts)}")
@@ -51,21 +62,20 @@ class ESearch:
         return hosts
 
     def __getattr__(self, item):
-        context = stack.top
+        context = _get_current_context()
 
         if context is not None:
-            if not hasattr(context, 'elasticsearch'):
-                hosts = self.configure_app(context)
-                auth = context.app.config.get('ELASTICSEARCH_HTTP_AUTH')
-                context.elasticsearch = Elasticsearch(hosts=hosts, http_auth=auth, **self.options)
+            hosts = self.configure_app(current_app)
+            auth = current_app.config.get('ELASTICSEARCH_HTTP_AUTH')
+            context._flask_esearch = Elasticsearch(hosts=hosts, http_auth=auth, **self.options)
 
-            return getattr(context.elasticsearch, item)
+            return getattr(context._flask_esearch, item)
         raise RuntimeError("No Application Context")
 
     def teardown(self, exception):
         """
         Tears down when the application context ends
         """
-        ctx = stack.top
-        if hasattr(ctx, 'elasticsearch'):
-            ctx.elasticsearch = None
+        context = _get_current_context()
+        if hasattr(context, '_flask_esearch'):
+            context._flask_esearch = None
